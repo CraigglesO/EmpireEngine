549848c97b724a7f855f39424f33c7c622f26049
oconnorct1 <oconnorct1@gmail.com>
2017-01-29 02:10:26 -0500

    lol

---
+++ test/test.js
@@ -0,0 +1,8 @@
+"use strict";
+const test = require("blue-tape");
+test("ut_metadata testings..", (t) => {
+    t.plan(6);
+    t.true(true);
+    t.end();
+});
+//# sourceMappingURL=test.js.map
---
+++ test/test.ts
@@ -0,0 +1,12 @@
+import * as test from "blue-tape";
+import Empire from "../Empire";
+
+// ut_metadata tests:
+test("ut_metadata testings..", (t) => {
+  t.plan(6);
+
+  t.true( true );
+
+  t.end();
+
+});
